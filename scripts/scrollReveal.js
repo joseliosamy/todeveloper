400a3818cc8101d6d8fdc15a7f332d674d93543c
Josélio Samy <joseliobackup@gmail.com>
2021-01-13 12:43:33 -0300

    new tools added

---
+++ scripts/scrollReveal.js
@@ -0,0 +1,34 @@
+const srTop = ScrollReveal({
+    origin: 'top',
+    distance: '100px',
+    duration: 2000,
+    reset: true
+});
+
+srTop.reveal('.box-landing');
+srTop.reveal('.landing-page .rc',{delay: 200});
+srTop.reveal('.about-page .rc',{delay: 200});
+srTop.reveal('.box-about h1',{delay:200});
+srTop.reveal('.box-about p');
+srTop.reveal('.box-about .rc',{delay:200});
+srTop.reveal('.downloads-page h1');
+srTop.reveal('.downloads-page .card',{delay:200});
+srTop.reveal('.multi-tools .tool-row h1',{delay:200});
+srTop.reveal('.multi-tools .tool-row h3');
+srTop.reveal('.multi-tools .tool-row p',{delay:800});
+srTop.reveal('.multi-tools .tool-row .rc',{delay:200});
+srTop.reveal('.multi-tools .tool-row small');
+srTop.reveal('.multi-tools .tool-row .button');
+srTop.reveal('.multi-tools .tool-row .socials');
+
+
+
+
+
+
+
+
+
+
+
+

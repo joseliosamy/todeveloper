529786662b0cff25b1b7f6ccb42649a7cae1c681
Josélio <joseliobackup@gmail.com>
2021-09-13 02:45:08 -0300

    estable v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+
+const Site = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #101010;
+  color: #d0d0d0;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Site></Site>;
 }
 
 export default App;
